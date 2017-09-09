d6c99c20553a31c7be0bd78b7f89f7135f2cb2fc
Terje Uglebakken <teug91@gmail.com>
2017-09-09 22:16:10 +0200

    Initial push

--- IdleRGB/App.xaml.cs
+++ IdleRGB/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using NotifyIcon = System.Windows.Forms.NotifyIcon;
 
 namespace IdleRGB
 {
@@ -13,5 +14,22 @@ namespace IdleRGB
     /// </summary>
     public partial class App : Application
     {
+        public static NotifyIcon icon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            App.icon = new NotifyIcon();
+            icon.Click += new EventHandler(Icon_Click);
+            icon.Icon = IdleRGB.Properties.Resources.bulb;
+            icon.Visible = true;
+
+            base.OnStartup(e);
+        }
+
+        private void Icon_Click(Object sender, EventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+        }
     }
 }
